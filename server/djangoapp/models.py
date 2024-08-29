2665fa0319bdc8a78e70cdecd71bc3d4df43aa88
sanahameed123 <sanahameedktl@gmail.com>
2024-08-29 19:00:26 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,6 +6,11 @@
 
 
 # Create your models here.
+from django.db import models
+
+class MyModel(models.Model):
+    field1 = models.CharField(max_length=100)
+    field2 = models.IntegerField()
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
