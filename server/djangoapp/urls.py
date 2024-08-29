cf1421eba7bd1faacd1a20e5baa34ccedebfa287
sanahameed123 <sanahameedktl@gmail.com>
2024-08-29 19:19:24 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,7 +4,22 @@ from django.conf import settings
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+  
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = 'djangoapp'
+urlpatterns = [
+        path(route='login', view=views.login_user, name='login'),
+    #
+    # path for login
+    # path(route='login', view=views.login_user, name='login'),
+
+    # path for dealer reviews view
+
+    # path for add a review view
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
     # path for login
     # path(route='login', view=views.login_user, name='login'),
